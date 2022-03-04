f940715994a6e2f1f48c25ed1b1e1d175cd3cd18
ar4220 <a.robert20@imperial.ac.uk>
2022-03-04 17:16:29 +0100

    clean code  and plot

--- learn_hypers_only.py
+++ learn_hypers_only.py
@@ -23,8 +23,8 @@ h = filter_amplitude*np.exp(-(1/(2*filter_length_scale))*(times_h)**2)
 # generate data
 gpc = GPC('RBF-RBF')
 times_x = torch.arange(0, N_data, 1).detach()
-x = gpc.sample_x(times_x, params = [amplitude_scale, length_scale], how_many=1)
-f = np.convolve(x.flatten(),h,mode='same')
+x = gpc.sample_x(times_x, params=[amplitude_scale, length_scale], how_many=1)
+f = np.convolve(x.flatten(), h, mode='same')
 y = f + sigma_noise*np.random.randn(len(times_x))
 idx = np.random.choice(len(times_x), 1000)
 times_y = times_x[idx]
@@ -42,20 +42,22 @@ for i in range(num_steps):
     print_loss(res, i)
 
 # visualize setting and results
-fig, ax = plt.subplots(2, 1, figsize=(10,8))
-ax[0].plot(times_x, x, label = 'x');
-ax[0].plot(times_x, f, label = 'f');
-ax[0].plot(times_y, y, '.r', label = 'y');
-ax[0].plot(times_h, 10*h, lw=3, label = 'h');
+fig, ax = plt.subplots(2, 1, figsize=(10, 8))
+ax[0].plot(times_x, x, label='x', color='orange')
+ax[0].plot(times_x, f, label='f', color='blue')
+ax[0].plot(times_y, y, '.r', label = 'y')
+ax[0].plot(times_h, 10*h, lw=3, label = 'h')
+ax[0].set_xlim([0, 500])
 ax[0].legend()
 # plot deconvolution with sparse representation and learnt parameters
 x_hat, V_hat, u_hat = gpc.forward(times_x)
-ax[1].plot(times_x, x_hat, label = 'deconv')
+ax[1].plot(times_x, x_hat, label='deconv', color='black')
 error_bars = np.sqrt(np.diag(V_hat))
 ax[1].fill_between(times_x, x_hat-2*error_bars,
                    x_hat+2*error_bars,
-                   label='error bars', alpha=0.5)
-ax[1].plot(times_x, x, label = 'true')
+                   label='error bars', alpha=0.5,
+                   color='black')
+ax[1].plot(times_x, x, label='x', color='orange')
 ax[1].plot(times_y, y, '.r', label='observations')
 ax[1].legend()
 ax[1].set_xlim([0, 500])
